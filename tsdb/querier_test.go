26d8cabfef7493854206e784ae05e199a04a4ca1
Bryan Boreham <bjboreham@gmail.com>
2022-06-28 16:03:26 +0100

    tsdb/index: use SimpleBuilder to create Labels
    This necessitates a change to the `tsdb.IndexReader` interface:
    `index.Reader` is used from multiple goroutines concurrently, so we
    can't have state in it.
    
    We do retain a `SimpleBuilder` in `blockBaseSeriesSet` which is
    iterator-like.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- tsdb/querier_test.go
+++ tsdb/querier_test.go
@@ -1265,7 +1265,7 @@ func (m mockIndex) SortedPostings(p index.Postings) index.Postings {
 	return index.NewListPostings(ep)
 }
 
-func (m mockIndex) Series(ref storage.SeriesRef, lset *labels.Labels, chks *[]chunks.Meta) error {
+func (m mockIndex) Series(ref storage.SeriesRef, builder *labels.SimpleBuilder, lset *labels.Labels, chks *[]chunks.Meta) error {
 	s, ok := m.series[ref]
 	if !ok {
 		return storage.ErrNotFound
@@ -1878,9 +1878,10 @@ func TestPostingsForMatchers(t *testing.T) {
 		p, err := PostingsForMatchers(ir, c.matchers...)
 		require.NoError(t, err)
 
+		var builder labels.SimpleBuilder
 		for p.Next() {
 			lbls := labels.Labels{}
-			require.NoError(t, ir.Series(p.At(), &lbls, &[]chunks.Meta{}))
+			require.NoError(t, ir.Series(p.At(), &builder, &lbls, &[]chunks.Meta{}))
 			if _, ok := exp[lbls.String()]; !ok {
 				t.Errorf("Evaluating %v, unexpected result %s", c.matchers, lbls.String())
 			} else {
@@ -2089,7 +2090,7 @@ func (m mockMatcherIndex) SortedPostings(p index.Postings) index.Postings {
 	return index.EmptyPostings()
 }
 
-func (m mockMatcherIndex) Series(ref storage.SeriesRef, lset *labels.Labels, chks *[]chunks.Meta) error {
+func (m mockMatcherIndex) Series(ref storage.SeriesRef, builder *labels.SimpleBuilder, lset *labels.Labels, chks *[]chunks.Meta) error {
 	return nil
 }
 
