55c4897be8401b3231a1a5e42be07d7b4414b816
Bryan Boreham <bjboreham@gmail.com>
2023-04-30 11:41:05 +0100

    tsdb tests: make work with SymbolTable labels
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- tsdb/index/index_test.go
+++ tsdb/index/index_test.go
@@ -206,7 +206,7 @@ func TestIndexRW_Postings(t *testing.T) {
 
 		require.NoError(t, err)
 		require.Equal(t, 0, len(c))
-		require.Equal(t, series[i], builder.Labels())
+		require.True(t, labels.Equal(series[i], builder.Labels()))
 	}
 	require.NoError(t, p.Err())
 
@@ -435,7 +435,7 @@ func TestPersistence_index_e2e(t *testing.T) {
 
 			err = mi.Series(expp.At(), &eBuilder, &expchks)
 			require.NoError(t, err)
-			require.Equal(t, eBuilder.Labels(), builder.Labels())
+			require.True(t, labels.Equal(eBuilder.Labels(), builder.Labels()))
 			require.Equal(t, expchks, chks)
 		}
 		require.False(t, expp.Next(), "Expected no more postings for %q=%q", p.Name, p.Value)
--- tsdb/wlog/checkpoint_test.go
+++ tsdb/wlog/checkpoint_test.go
@@ -30,6 +30,7 @@ import (
 	"github.com/prometheus/prometheus/model/labels"
 	"github.com/prometheus/prometheus/tsdb/chunks"
 	"github.com/prometheus/prometheus/tsdb/record"
+	"github.com/prometheus/prometheus/util/deepequal"
 )
 
 func TestLastCheckpoint(t *testing.T) {
@@ -287,7 +288,7 @@ func TestCheckpoint(t *testing.T) {
 				{Ref: 2, Labels: labels.FromStrings("a", "b", "c", "2")},
 				{Ref: 4, Labels: labels.FromStrings("a", "b", "c", "4")},
 			}
-			require.Equal(t, expectedRefSeries, series)
+			deepequal.RequireEqual(t, expectedRefSeries, series)
 
 			expectedRefMetadata := []record.RefMetadata{
 				{Ref: 0, Unit: fmt.Sprintf("%d", last-100), Help: fmt.Sprintf("%d", last-100)},
